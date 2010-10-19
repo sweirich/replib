27f22492a41932dac1d8e384eff377c8ecd25296
byorgey <byorgey@d9fc01f3-7ead-27f9-7cc3-93ae016f4439>
2010-10-19 21:44:27 +0000

    start cleaning up Perm module

--- trunk/Data/RepLib/Bind/Perm.hs
+++ trunk/Data/RepLib/Bind/Perm.hs
@@ -1,58 +1,79 @@
--- | Permutations 
-module Data.RepLib.Bind.Perm (Perm, empty, single, 
-                         join, apply, support, isid, joins) where
 
-{- A slow, but hopefully correct implementation of permutations -}
+----------------------------------------------------------------------
+-- |
+-- Module      :  Data.RepLib.Bind.Perm
+-- Copyright   :  ???
+-- License     :  ???
+--
+-- Maintainer  :  Stephanie Weirich <sweirich@cis.upenn.edu>
+-- Stability   :  experimental
+-- Portability :  portable
+--
+-- A slow, but hopefully correct implementation of permutations.
+--
+----------------------------------------------------------------------
+
+module Data.RepLib.Bind.Perm (
+    Perm, empty, single,
+    join, apply, support, isid, joins
+  ) where
 
 import Data.List
 import System.IO.Unsafe
 
--- | A Permutation of values of type 'a'
+-- | A permutation of values of type 'a'.
 data Perm a = Empty
             | Single a a
-            | Join (Perm a) (Perm a) deriving (Show)
+            | Join (Perm a) (Perm a)
+  deriving (Show)
 
 instance Ord a => Eq (Perm a) where
    p1 == p2 = all (\x -> apply p1 x == apply p2 x) (support p1)
               && support p1 `seteq` support p2
 
--- | Create an identity permutation which leaves its argument unchanged
+-- | Create an identity permutation which has no effect.
 empty :: Eq a => Perm a
-empty  = Empty   
+empty  = Empty
 
--- | Create a singleton permutation
--- "single x y" exchanges x and y but leaves all other values alone
-single :: Eq a => a -> a -> Perm a 
-single = Single  
+-- | Create a singleton permutation.  @'single' x y@ exchanges @x@ and
+--   @y@ but leaves all other values alone.
+single :: Eq a => a -> a -> Perm a
+single = Single
 
--- | Compose two permutations together
-join :: Eq a => Perm a -> Perm a -> Perm a 
+-- XXX replace with Monoid instance
+-- | Compose two permutations.
+join :: Eq a => Perm a -> Perm a -> Perm a
 join Empty p = p
 join p Empty = p
 join p1 p2   = Join p1 p2
 
--- | Permute a given object
-apply :: Eq a => Perm a -> a -> a 
-apply Empty a = a
+-- | Apply a permutation to an object, possibly resulting in a
+--   different object.
+apply :: Eq a => Perm a -> a -> a
+apply Empty        a = a
 apply (Single x y) a = sw x y a
 apply (Join p1 p2) a = apply p1 (apply p2 a)
 
+-- | Possibly split a permutation into a single swap and a remaining
+--   permutation.  Return 'Nothing' if the permutation is the identity.
 first :: Perm a -> Maybe ((a,a), Perm a)
-first Empty = Nothing
+first Empty        = Nothing
 first (Single a b) = Just ((a,b), Empty)
-first (Join p1 p2) = case first p1 of 
-   Nothing -> first p2 
+first (Join p1 p2) = case first p1 of
+   Nothing -> first p2
    Just (s, p1') -> Just (s, Join p1' p2)
 
-names :: Eq a => Perm a -> [ a ]
-names Empty = []
+-- XXX change 'names' to a better name?
+-- | Return a list of all the objects mentioned by a permutation.
+names :: Eq a => Perm a -> [a]
+names Empty        = []
 names (Single x y) = [x , y]
-names (Join p1 p2) = 
+names (Join p1 p2) =
    names p1 ++ names p2
 
--- | The list of objects that are changed by this permutation
-support :: Eq a => Perm a -> [ a ] 
-support p = [ x | x <- names p, not (apply p x == x) ] 
+-- | Return a list of all objects that are changed by this permutation.
+support :: Eq a => Perm a -> [a]
+support p = [ x | x <- names p, not (apply p x == x) ]
 
 -- | Is this permutation the identity?
 isid :: Eq a => Perm a -> Bool
@@ -60,7 +81,7 @@ isid Empty = True
 isid (Single x y) = x == y
 isid p = support p == []
 
--- | Join a list of permutations together
+-- | Compose an entire list of permutations.
 joins :: Eq a => [Perm a] -> Perm a
 joins = foldl join Empty
 
@@ -68,8 +89,8 @@ seteq :: Ord a => [a] -> [a] -> Bool
 seteq x y = nub (sort x) == nub (sort y)
 
 sw :: Eq a => a -> a -> a -> a
-sw x y s | x==s = y 
-         | y==s = x 
+sw x y s | x==s = y
+         | y==s = x
          | True = s
 
 assert :: String -> Bool -> IO ()
@@ -86,7 +107,7 @@ do_tests =
 tests_apply = do
   assert "a1" $ apply Empty 1 == 1
   assert "a2" $ apply (Single 1 2) 1 == 2
-  assert "a3" $ apply (Single 2 1) 1 == 2 
+  assert "a3" $ apply (Single 2 1) 1 == 2
   assert "a4" $ apply (Join (Single 1 2) (Single 2 1)) 1 == 1
 
 tests_isid = do
@@ -95,7 +116,7 @@ tests_isid = do
   assert "i3" $ isid (Single 1 1) == True
   assert "i4" $ isid (Join (Single 1 2) (Single 1 2)) == True
   assert "i5" $ isid (Join (Single 1 2) (Single 2 1)) == True
-  assert "i6" $ isid (Join (Single 1 2) (Single 3 2)) == False 
+  assert "i6" $ isid (Join (Single 1 2) (Single 3 2)) == False
 
 tests_support = do
   assert "s1" $ support (Empty :: Perm Int) `seteq` []
@@ -103,10 +124,10 @@ tests_support = do
   assert "s3" $ support (Single 1 1) `seteq` []
   assert "s4" $ support (Join (Single 1 2) (Single 1 2)) `seteq` []
   assert "s5" $ support (Join (Single 1 2) (Single 2 1)) `seteq` []
-  assert "s6" $ support (Join (Single 1 2) (Single 3 2)) `seteq` [1,2,3] 
+  assert "s6" $ support (Join (Single 1 2) (Single 3 2)) `seteq` [1,2,3]
 
 -- need a generator for perms for this to work
 {-
-p_isid p = forAll (arbitrary :: Gen Int) $ \x -> 
+p_isid p = forAll (arbitrary :: Gen Int) $ \x ->
     isid p == True ==> apply p x == x
 -}
