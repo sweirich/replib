bc8428d93d38785ceb684906ba2622c1fdbcf079
byorgey <byorgey@d9fc01f3-7ead-27f9-7cc3-93ae016f4439>
2010-10-21 16:32:25 +0000

    replace Perm with new shiny fingertree implementation

--- Data/RepLib/Bind/Perm.hs
+++ Data/RepLib/Bind/Perm.hs
@@ -1,15 +1,15 @@
-
+{-# LANGUAGE MultiParamTypeClasses #-}
 ----------------------------------------------------------------------
 -- |
 -- Module      :  Data.RepLib.Bind.Perm
--- Copyright   :  ???
--- License     :  ???
+-- Copyright   :  (c) 2010, The University of Pennsylvania
+-- License     :  BSD
 --
 -- Maintainer  :  Stephanie Weirich <sweirich@cis.upenn.edu>
 -- Stability   :  experimental
 -- Portability :  portable
 --
--- A slow, but hopefully correct implementation of permutations.
+-- An abstract type of general permutations.
 --
 ----------------------------------------------------------------------
 
@@ -17,24 +17,52 @@ module Data.RepLib.Bind.Perm (
     Perm, single, (<>), apply, support, isid
   ) where
 
-import Data.Monoid
 import Data.List
+import qualified Data.Set as S
+import Data.FingerTree ((><))
+import qualified Data.FingerTree as FT
+
+import Data.Monoid
+import qualified Data.Foldable as F
+
 import System.IO.Unsafe
 
+
 -- | A permutation of values of type 'a'.
-data Perm a = Empty
-            | Single a a
-            | Join (Perm a) (Perm a)
-  deriving (Show)
+newtype Perm a = Perm (FT.FingerTree (S.Set a) (Swap a))
 
 -- Permutations are represented as a list of swaps, to be applied from
--- right to left.  The list is stored as a join-list for efficiency.
+-- right to left.  The list is stored in a fingertree for efficiency.
+-- At the nodes of the fingertree we cache the set of objects mentioned
+-- by the permutation, so we don't have to recompute it every time we
+-- want to compute the support.
+--
+-- XXX todo: design a monoid to compute and cache the support
+-- directly?
+
+-- | Data structure representing a single swap of two elements.
+--   Invariant: the two elements are not equal.
+data Swap a = Swap a a
+  deriving (Eq, Ord, Show)
+
+-- | Apply a swap.
+swap :: Eq a => Swap a -> a -> a
+swap (Swap x y) s
+    | x == s    = y
+    | y == s    = x
+    | otherwise = s
+
+-- | 'Swap's are \"measured\" by the set of objects that they mention;
+--   this allows fingertrees of 'Swap's to cache the set of objects
+--   mentioned by all the 'Swap's.
+instance Ord a => FT.Measured (S.Set a) (Swap a) where
+  measure (Swap x y) = S.fromList [x,y]
 
 -- | Two permutations are equal if they have the same behavior when
 --   applied, i.e. they send the same objects to the same objects.
 instance Ord a => Eq (Perm a) where
-   p1 == p2 = all (\x -> apply p1 x == apply p2 x) (support p1)
-              && support p1 `seteq` support p2
+   p1 == p2 = F.all (\x -> apply p1 x == apply p2 x) (support p1)
+              && support p1 == support p2
 
 -- | Permutations form a monoid:
 --
@@ -42,62 +70,54 @@ instance Ord a => Eq (Perm a) where
 --
 --     * 'mappend' is composition of permutations
 --       (the right-hand permutation is applied first).
-instance Monoid (Perm a) where
-  mempty            = Empty
-  Empty `mappend` p = p
-  p `mappend` Empty = p
-  p1 `mappend` p2   = Join p1 p2
+instance Ord a => Monoid (Perm a) where
+  mempty                        = Perm FT.empty
+  (Perm p1) `mappend` (Perm p2) = Perm $ p1 >< p2
 
 -- | Create a singleton permutation.  @'single' x y@ exchanges @x@ and
 --   @y@ but leaves all other values alone.
-single :: Eq a => a -> a -> Perm a
-single = Single
+single :: Ord a => a -> a -> Perm a
+single x y
+    | x == y    = mempty
+    | otherwise = Perm $ FT.singleton (Swap x y)
 
 -- | Compose two permutations.  The right-hand permutation will be
 --   applied first.
-(<>) :: Perm a -> Perm a -> Perm a
+(<>) :: Ord a => Perm a -> Perm a -> Perm a
 (<>) = mappend
 
 -- | Apply a permutation to an object, possibly resulting in a
 --   different object.
 apply :: Eq a => Perm a -> a -> a
-apply Empty        a = a
-apply (Single x y) a = sw x y a
-apply (Join p1 p2) a = apply p1 (apply p2 a)
+apply (Perm p) a = F.foldr swap a p
+  -- F.foldr (from Data.Foldable) works on a fingertree just like
+  -- Data.List.foldr works on a list.  So, for example, if p
+  -- corresponds to [(1,2), (1,3), (4,5)] we get
+  --
+  --   swap (1,2) (swap (1,3) (swap (4,5) a))
 
--- | Possibly split a permutation into a single swap and a remaining
---   permutation.  Return 'Nothing' if the permutation is the identity.
-first :: Perm a -> Maybe ((a,a), Perm a)
-first Empty        = Nothing
-first (Single a b) = Just ((a,b), Empty)
-first (Join p1 p2) = case first p1 of
-   Nothing -> first p2
-   Just (s, p1') -> Just (s, Join p1' p2)
+-- | Return a set of all the objects mentioned by a permutation.  Note
+--   that this is different from the set of objects *affected* by the
+--   permutation (for that, see 'support').  For example if we had the
+--   permutation
+--
+--     [(1,2), (2,1)]
+--
+--   'mentions' would return the set [1,2], whereas the 'support'
+--   would be the empty set.
+mentions :: Ord a => Perm a -> S.Set a
+mentions (Perm p) = FT.measure p
+  -- The set of such objects is cached at the root of the fingertree.
 
--- | Return a list of all the objects mentioned by a permutation.
-names :: Eq a => Perm a -> [a]
-names Empty        = []
-names (Single x y) = [x , y]
-names (Join p1 p2) =
-   names p1 ++ names p2
-
--- | Return a list of all objects that are changed by this permutation.
-support :: Eq a => Perm a -> [a]
-support p = [ x | x <- names p, not (apply p x == x) ]
+-- | Return a set of all objects that are changed by this permutation.
+support :: Ord a => Perm a -> S.Set a
+support p = S.filter (\x -> apply p x /= x) (mentions p)
 
 -- | Is this permutation the identity?
-isid :: Eq a => Perm a -> Bool
-isid Empty = True
-isid (Single x y) = x == y
-isid p = support p == []
+isid :: Ord a => Perm a -> Bool
+isid = S.null . support
 
-seteq :: Ord a => [a] -> [a] -> Bool
-seteq x y = nub (sort x) == nub (sort y)
-
-sw :: Eq a => a -> a -> a -> a
-sw x y s | x==s = y
-         | y==s = x
-         | True = s
+-- Testing
 
 assert :: String -> Bool -> IO ()
 assert s True = return ()
@@ -111,26 +131,26 @@ do_tests =
      tests_support
 
 tests_apply = do
-  assert "a1" $ apply Empty 1 == 1
-  assert "a2" $ apply (Single 1 2) 1 == 2
-  assert "a3" $ apply (Single 2 1) 1 == 2
-  assert "a4" $ apply (Join (Single 1 2) (Single 2 1)) 1 == 1
+  assert "a1" $ apply mempty 1 == 1
+  assert "a2" $ apply (single 1 2) 1 == 2
+  assert "a3" $ apply (single 2 1) 1 == 2
+  assert "a4" $ apply ((<>) (single 1 2) (single 2 1)) 1 == 1
 
 tests_isid = do
-  assert "i1" $ isid (Empty :: Perm Int) == True
-  assert "i2" $ isid (Single 1 2) == False
-  assert "i3" $ isid (Single 1 1) == True
-  assert "i4" $ isid (Join (Single 1 2) (Single 1 2)) == True
-  assert "i5" $ isid (Join (Single 1 2) (Single 2 1)) == True
-  assert "i6" $ isid (Join (Single 1 2) (Single 3 2)) == False
+  assert "i1" $ isid (mempty :: Perm Int) == True
+  assert "i2" $ isid (single 1 2) == False
+  assert "i3" $ isid (single 1 1) == True
+  assert "i4" $ isid ((<>) (single 1 2) (single 1 2)) == True
+  assert "i5" $ isid ((<>) (single 1 2) (single 2 1)) == True
+  assert "i6" $ isid ((<>) (single 1 2) (single 3 2)) == False
 
 tests_support = do
-  assert "s1" $ support (Empty :: Perm Int) `seteq` []
-  assert "s2" $ support (Single 1 2) `seteq` [1,2]
-  assert "s3" $ support (Single 1 1) `seteq` []
-  assert "s4" $ support (Join (Single 1 2) (Single 1 2)) `seteq` []
-  assert "s5" $ support (Join (Single 1 2) (Single 2 1)) `seteq` []
-  assert "s6" $ support (Join (Single 1 2) (Single 3 2)) `seteq` [1,2,3]
+  assert "s1" $ support (mempty :: Perm Int) == S.empty
+  assert "s2" $ support (single 1 2) == S.fromList [1,2]
+  assert "s3" $ support (single 1 1) == S.empty
+  assert "s4" $ support ((<>) (single 1 2) (single 1 2)) == S.empty
+  assert "s5" $ support ((<>) (single 1 2) (single 2 1)) == S.empty
+  assert "s6" $ support ((<>) (single 1 2) (single 3 2)) == S.fromList [1,2,3]
 
 -- need a generator for perms for this to work
 {-
