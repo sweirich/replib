fc3511a9850d8a3f1105dd1a82c40d576aa83c4b
byorgey <byorgey@d9fc01f3-7ead-27f9-7cc3-93ae016f4439>
2010-12-14 20:47:27 +0000

    LF type checker is finished, after much repeated refactoring and hand-wringing.  now for the parser.

--- trunk/examples/LF.hs
+++ trunk/examples/LF.hs
@@ -11,9 +11,12 @@
            , UndecidableInstances
            , TypeSynonymInstances
            , TypeFamilies
+           , GeneralizedNewtypeDeriving
   #-}
 
 module LF where
+
+import Prelude hiding (lookup)
 
 import Generics.RepLib.Bind.LocallyNameless
 import Generics.RepLib
@@ -28,6 +31,11 @@ import Control.Monad.Reader
 import Control.Applicative hiding (many)
 
 import qualified Data.Map as M
+import qualified Data.Set as S
+
+------------------------------
+-- Syntax --------------------
+------------------------------
 
 -- Kinds
 data Kind = KPi (Bind (Name Tm, Annot Ty) Kind) -- {x:ty} k
@@ -44,8 +52,10 @@ data Ty   = TyPi (Bind (Name Tm, Annot Ty) Ty)  -- {x:ty} ty
 data Tm   = Lam (Bind (Name Tm, Annot Ty) Tm)   -- [x:ty] tm
           | TmApp Tm Tm                         -- tm tm
           | TmVar (Name Tm)                     -- x
-          | TmConst (Name Tm)                   -- c
   deriving Show
+  -- Harper and Pfennig distinguish between term *variables* and term
+  -- *constants*, but for our purposes there is no need to distinguish
+  -- between them.
 
 $(derive [''Kind, ''Ty, ''Tm])
 
@@ -64,39 +74,18 @@ instance Subst Tm Tm where
   isvar (TmVar v) = Just (v, id)
   isvar _         = Nothing
 
--- TODO: do this more simply, without binding.
--- A declaration is either a type constant declaration (a name and a kind)
--- or a term constant declaration (a name, type, and optional definition).
-data Decl = DeclTy (Name Ty) (Annot Kind)
-          | DeclTm (Name Tm) (Annot Ty) (Maybe (Annot Tm))  -- is this right?
+-- A declaration is either
+--   * a type constant declaration (a name and a kind),
+--   * a term constant declaration (a name, type, and optional definition), or
+--   * a fixity/precedence declaration.
+data Decl = DeclTy (Name Ty) Kind
+          | DeclTm (Name Tm) Ty (Maybe Tm)
+          | DeclInfix (Name Tm) Assoc Int
 
--- A program is a sequence of declarations, where each name is bound
--- in the remainder of the program.  We parse files into a Prog, then
--- take the Prog apart piece by piece to typecheck it.  This way we
--- get the full power of the binding library to help us with name
--- shadowing and so on.
-data Prog = Nil
-          | Cons (Bind Decl Prog)
+data Assoc = L | R
 
--- Signatures/contexts
-
--- todo: don't distinguish between signatures and contexts?
-
--- A type signature is a mapping from type variables to kinds.
-type TySig = M.Map (Name Ty) Kind
-
--- A term signature is a mappint from term variables to types and
--- (optionally) a definition.
-type TmSig = M.Map (Name Tm) (Ty, Maybe Tm)
-
--- A signature is a pair of a type and term signature.
-type Sig = (TySig, TmSig)
-
--- A context is a mapping from term variables to types.
-type Context = M.Map (Name Tm) Ty
-
-extend :: Ord k => M.Map k a -> k -> a -> M.Map k a
-extend g x a = M.insert x a g
+-- A program is a sequence of declarations.
+type Prog = [Decl]
 
 --------------------
 -- Erasure ---------
@@ -113,8 +102,7 @@ data STy   = STyConst (Name Ty)
                        -- use 'aeq' from
                        -- Generics.RepLib.Bind.LocallyNameless.
 
--- Simple contexts map variables to simple types.
-type SContext = M.Map (Name Tm) STy
+$(derive [''SKind, ''STy])
 
 class Erasable t where
   type Erased t :: *
@@ -135,9 +123,95 @@ instance Erasable Ty where
   erase (TyApp ty _)  = erase ty
   erase (TyConst c)   = STyConst c
 
-instance Erasable Context where
-  type Erased Context = SContext
+instance Erasable Tm where
+  type Erased Tm = Tm
+  erase t = t
+
+instance (Erasable a, Erasable b) => Erasable (a,b) where
+  type Erased (a,b) = (Erased a, Erased b)
+  erase (x,y) = (erase x, erase y)
+
+------------------------------
+-- Signatures/contexts -------
+------------------------------
+
+data Context tm ty = C (M.Map (Name Tm) tm) (M.Map (Name Ty) ty)
+
+emptyCtx :: Context tm ty
+emptyCtx = C M.empty M.empty
+
+extendTm :: Name Tm -> tm -> Context tm ty -> Context tm ty
+extendTm x t (C tm ty) = C (M.insert x t tm) ty
+
+extendTy :: Name Ty -> ty -> Context tm ty -> Context tm ty
+extendTy x k (C tm ty) = C tm (M.insert x k ty)
+
+lookupTm :: (MonadPlus m, MonadReader (Context tm ty) m)
+         => Name Tm -> m tm
+lookupTm x = ask >>= \(C tm _) -> embedMaybe (M.lookup x tm)
+
+lookupTy :: (MonadPlus m, MonadReader (Context tm ty) m)
+         => Name Ty -> m ty
+lookupTy x = ask >>= \(C _ ty) -> embedMaybe (M.lookup x ty)
+
+embedMaybe :: (MonadPlus m) => Maybe a -> m a
+embedMaybe = maybe mzero return
+
+instance Erasable a => Erasable (M.Map k a) where
+  type Erased (M.Map k a) = M.Map k (Erased a)
   erase = M.map erase
+
+instance (Erasable tm, Erasable ty) => Erasable (Context tm ty) where
+  type Erased (Context tm ty) = Context (Erased tm) (Erased ty)
+  erase (C tm ty) = C (erase tm) (erase ty)
+
+instance (Erasable a) => Erasable (Maybe a) where
+  type Erased (Maybe a) = Maybe (Erased a)
+  erase = fmap erase
+
+type Ctx  = Context (Ty, Maybe Tm) Kind
+type SCtx = Erased Ctx
+
+withTmBinding :: (MonadReader (Context (tm, Maybe Tm) ty) m, LFresh m)
+              => Name Tm -> tm -> m r -> m r
+withTmBinding x b = do
+  avoid [AnyName x] . local (extendTm x (b, Nothing))
+
+withTmDefn :: (MonadReader (Context tm ty) m, LFresh m)
+           => Name Tm -> tm -> m r -> m r
+withTmDefn x b = do
+  avoid [AnyName x] . local (extendTm x b)
+
+withTyBinding :: (MonadReader (Context tm ty) m, LFresh m)
+              => Name Ty -> ty -> m r -> m r
+withTyBinding x b = do
+  avoid [AnyName x] . local (extendTy x b)
+
+------------------------------
+-- Typechecking monad --------
+------------------------------
+
+newtype TcM ctx a = TcM { unTcM :: MaybeT (ReaderT ctx FreshM) a }
+  deriving (Functor, Applicative, Monad, MonadReader ctx, MonadPlus, LFresh)
+
+getTcMAvoids :: TcM ctx (S.Set AnyName)
+getTcMAvoids = TcM . lift . lift $ getAvoids
+
+-- | Continue a TcM computation, given a context and set of names to
+--   avoid.
+contTcM :: TcM ctx a -> ctx -> S.Set AnyName -> Maybe a
+contTcM (TcM m) c nms = flip contFreshM nms . flip runReaderT c . runMaybeT $ m
+
+-- | Run a TcM computation in an empty context.
+runTcM :: TcM (Context tm ty) a -> Maybe a
+runTcM m = contTcM m emptyCtx S.empty
+
+-- | Run a subcomputation with an erased context.
+withErasedCtx :: TcM SCtx a -> TcM Ctx a
+withErasedCtx m = do
+  c <- ask
+  nms <- getTcMAvoids
+  embedMaybe $ contTcM m (erase c) nms
 
 ------------------------------
 -- Weak head reduction -------
@@ -152,202 +226,164 @@ instance LFresh m => LFresh (ReaderT e m) where
   lfresh    = ReaderT . const . lfresh
   avoid nms = ReaderT . fmap (avoid nms) . runReaderT
 
--- Reduce a term to weak-head normal form, if it is head-reducible.
---   (Note, this fails if the term is already in WHNF.)
-whr :: (LFresh m, MonadPlus m)
+-- Reduce a term to weak-head normal form, or return it unchanged if
+-- it is not head-reducible.  Works in erased or unerased contexts.
+whr :: (LFresh m, MonadReader (Context (a,Maybe Tm) b) m, MonadPlus m)
     => Tm -> m Tm
+whr (TmVar a) = (do
+  (_, Just defn) <- lookupTm a
+  whr defn)
+  `mplus`
+  return (TmVar a)
+
 whr (TmApp (Lam b) m1) =
   lunbind b $ \((x,_),m2) ->
-    return $ subst x m1 m2
+    whr $ subst x m1 m2
 
 whr (TmApp m1 m2) = TmApp `liftM` whr m1 `ap` return m2
 
-whr _ = mzero
-
--- Reduce a term to weak-head normal form, or return it unchanged if
--- it is not head-reducible.
-wh :: (LFresh m, MonadPlus m)
-   => Tm -> m Tm
-wh t = whr t `mplus` return t
+whr t = return t
 
 ------------------------------
 -- Term equality -------------
 ------------------------------
 
--- todo: expand in whr, not in tmEqS.
-
 -- Type-directed term equality.  In context Delta, is M <==> N at
 -- simple type tau?
-tmEq :: (LFresh m, MonadPlus m, MonadReader Sig m)
-     => SContext -> Tm -> Tm -> STy -> m ()
-tmEq delta m n t = do
-  m' <- wh m
-  n' <- wh n
-  tmEq' delta m' n' t
+tmEq :: (LFresh m, MonadPlus m, MonadReader SCtx m)
+     => Tm -> Tm -> STy -> m ()
+tmEq m n t = do
+  m' <- whr m
+  n' <- whr n
+  tmEq' m' n' t
 
   -- XXX todo: might be nice to have 'lfresh' and 'lfreshen', the
   -- first NOT taking an argument
 
   -- XXX todo: need nicer way of doing "string2Name"
 -- Type-directed term equality on terms in WHNF
-tmEq' :: (LFresh m, MonadPlus m, MonadReader Sig m)
-      => SContext -> Tm -> Tm -> STy -> m ()
-tmEq' delta m n (STyArr t1 t2) = do
+tmEq' :: (LFresh m, MonadPlus m, MonadReader SCtx m)
+      => Tm -> Tm -> STy -> m ()
+tmEq' m n (STyArr t1 t2) = do
   x <- lfresh (string2Name "_x")
-  avoid [AnyName x] $
-    tmEq' (extend delta x t1) (TmApp m (TmVar x)) (TmApp n (TmVar x)) t2
-tmEq' delta m n a@(STyConst {}) = do
-  a' <- tmEqS delta m n
+  withTmBinding x t1 $
+    tmEq' (TmApp m (TmVar x)) (TmApp n (TmVar x)) t2
+tmEq' m n a@(STyConst {}) = do
+  a' <- tmEqS m n
   guard $ a == a'
 
-embedMaybe :: (MonadPlus m) => Maybe a -> m a
-embedMaybe = maybe mzero return
+-- Structural term equality.  Check whether two terms in WHNF are
+-- structurally equal, and return their "approximate type" if so.
+tmEqS :: (LFresh m, MonadPlus m, MonadReader SCtx m)
+      => Tm -> Tm -> m STy
 
--- Structural term equality.  Check whether two terms are structurally
--- equal, and return their "approximate type" if so.
-tmEqS :: (LFresh m, MonadPlus m, MonadReader Sig m)
-      => SContext -> Tm -> Tm -> m STy
-
--- First, expand out term constants with definitions.
-tmEqS delta a b = do
-  a' <- expand a
-  b' <- expand b
-  tmEqS' delta a' b'
-
-expand :: (MonadPlus m, MonadReader Sig m)
-       => Tm -> m Tm
-expand (TmConst a) =
-  (do
-     tmSig <- asks snd
-     (_, Just defn) <- embedMaybe $ M.lookup a tmSig
-     expand defn
-  )
-  `mplus` return (TmConst a)
-expand b = return b
-
--- Structural term equality, with the precondition that neither term
--- is a constant with an associated definition.
-tmEqS' :: (LFresh m, MonadPlus m, MonadReader Sig m)
-       => SContext -> Tm -> Tm -> m STy
-tmEqS' delta (TmVar x) (TmVar y) = do
-  guard $ x == y
-  embedMaybe $ M.lookup x delta
-
-  -- Guaranteed that these term constants have no definition.
-tmEqS' _ (TmConst a) (TmConst b) = do
+tmEqS (TmVar a) (TmVar b) = do
   guard $ a == b
-  tmSig <- asks snd
-  (tyA,_) <- embedMaybe $ M.lookup a tmSig
-  return $ erase tyA
+  (tyA,_) <- lookupTm a
+  return tyA
 
-tmEqS' delta (TmApp m1 m2) (TmApp n1 n2) = do
-  STyArr t2 t1 <- tmEqS delta m1 n1
-  tmEq delta m2 n2 t2
+tmEqS (TmApp m1 m2) (TmApp n1 n2) = do
+  STyArr t2 t1 <- tmEqS m1 n1
+  tmEq m2 n2 t2
   return t1
 
-tmEqS' _ _ _ = mzero
+tmEqS _ _ = mzero
 
 ------------------------------
 -- Type equality -------------
 ------------------------------
 
 -- Kind-directed type equality.
-tyEq :: (LFresh m, MonadPlus m, MonadReader Sig m)
-     => SContext -> Ty -> Ty -> SKind -> m ()
+tyEq :: (LFresh m, MonadPlus m, MonadReader SCtx m)
+     => Ty -> Ty -> SKind -> m ()
 
-tyEq delta (TyPi bnd1) (TyPi bnd2) SKType =
+tyEq (TyPi bnd1) (TyPi bnd2) SKType =
   lunbind bnd1 $ \((x, Annot a1), a2) ->
   lunbind bnd2 $ \((_, Annot b1), b2) -> do
-    tyEq delta a1 b1 SKType
-    tyEq (extend delta x (erase a1)) a2 b2 SKType
+    tyEq a1 b1 SKType
+    withTmBinding x (erase a1) $ tyEq a2 b2 SKType
 
-tyEq delta a b SKType = do
-  t <- tyEqS delta a b
+tyEq a b SKType = do
+  t <- tyEqS a b
   guard $ t == SKType
 
-tyEq delta a b (SKArr t k) = do
+tyEq a b (SKArr t k) = do
   x <- lfresh (string2Name "_x")
-  avoid [AnyName x] $
-    tyEq (extend delta x t) (TyApp a (TmVar x)) (TyApp b (TmVar x)) k
+  withTmBinding x t $ tyEq (TyApp a (TmVar x)) (TyApp b (TmVar x)) k
 
 -- Structural type equality.
-tyEqS :: (LFresh m, MonadPlus m, MonadReader Sig m)
-      => SContext -> Ty -> Ty -> m SKind
-tyEqS _ (TyConst a) (TyConst b) = do
+tyEqS :: (LFresh m, MonadPlus m, MonadReader SCtx m)
+      => Ty -> Ty -> m SKind
+tyEqS (TyConst a) (TyConst b) = do
   guard $ a == b
-  tySig <- asks fst
-  erase `liftM` (embedMaybe $ M.lookup a tySig)
+  lookupTy a
 
-tyEqS delta (TyApp a m) (TyApp b n) = do
-  SKArr t k <- tyEqS delta a b
-  tmEq delta m n t
+tyEqS (TyApp a m) (TyApp b n) = do
+  SKArr t k <- tyEqS a b
+  tmEq m n t
   return k
 
-tyEqS _ _ _ = mzero
+tyEqS _ _ = mzero
 
 ------------------------------
 -- Kind equality -------------
 ------------------------------
 
 -- Algorithmic kind equality.
-kEq :: (LFresh m, MonadPlus m, MonadReader Sig m)
-    => SContext -> Kind -> Kind -> m ()
+kEq :: (LFresh m, MonadPlus m, MonadReader SCtx m)
+    => Kind -> Kind -> m ()
 
-kEq _ Type Type = return ()
+kEq Type Type = return ()
 
-kEq delta (KPi bnd1) (KPi bnd2) =
+kEq (KPi bnd1) (KPi bnd2) =
   lunbind bnd1 $ \((x, Annot a), k) ->
   lunbind bnd2 $ \((_, Annot b), l) -> do
-    tyEq delta a b SKType
-    kEq (extend delta x (erase a)) k l
+    tyEq a b SKType
+    withTmBinding x (erase a) $ kEq k l
 
-kEq _ _ _ = mzero
+kEq _ _ = mzero
 
 ------------------------------
 -- Type checking -------------
 ------------------------------
 
 -- Compute the type of a term.
-tyCheck :: (LFresh m, MonadPlus m, MonadReader Sig m)
-        => Context -> Tm -> m Ty
-tyCheck gamma (TmVar x)     = embedMaybe $ M.lookup x gamma
-tyCheck _     (TmConst c)   = liftM fst . embedMaybe . M.lookup c =<< asks snd
-tyCheck gamma (TmApp m1 m2) = do
-  TyPi bnd <- tyCheck gamma m1
-  a2       <- tyCheck gamma m2
+tyCheck :: Tm -> TcM Ctx Ty
+tyCheck (TmVar x)     = liftM fst $ lookupTm x
+tyCheck (TmApp m1 m2) = do
+  TyPi bnd <- tyCheck m1
+  a2       <- tyCheck m2
   lunbind bnd $ \((x, Annot a2'), a1) -> do
-    tyEq (erase gamma) a2' a2 SKType
+    withErasedCtx $ tyEq a2' a2 SKType
     return $ subst x m2 a1
-tyCheck gamma (Lam bnd) =
+tyCheck (Lam bnd) =
   lunbind bnd $ \((x, Annot a1), m2) -> do
-    Type <- kCheck gamma a1
-    a2   <- tyCheck (extend gamma x a1) m2
+    Type <- kCheck a1
+    a2   <- withTmBinding x a1 $ tyCheck m2
     return $ TyPi (bind (x, Annot a1) a2)
 
 -- Compute the kind of a type.
-kCheck :: (LFresh m, MonadPlus m, MonadReader Sig m)
-       => Context -> Ty -> m Kind
-kCheck _     (TyConst a) = embedMaybe . M.lookup a =<< asks fst
-kCheck gamma (TyApp a m) = do
-  KPi bnd <- kCheck gamma a
-  b       <- tyCheck gamma m
+kCheck :: Ty -> TcM Ctx Kind
+kCheck (TyConst a) = lookupTy a
+kCheck (TyApp a m) = do
+  KPi bnd <- kCheck a
+  b       <- tyCheck m
   lunbind bnd $ \((x, Annot b'), k) -> do
-    tyEq (erase gamma) b' b SKType
+    withErasedCtx $ tyEq b' b SKType
     return $ subst x m k
-kCheck gamma (TyPi bnd) =
+kCheck (TyPi bnd) =
   lunbind bnd $ \((x, Annot a1), a2) -> do
-    Type <- kCheck gamma a1
-    Type <- kCheck (extend gamma x a1) a2
+    Type <- kCheck a1
+    Type <- withTmBinding x a1 $ kCheck a2
     return Type
 
 -- Check the validity of a kind.
-sortCheck :: (LFresh m, MonadPlus m, MonadReader Sig m)
-          => Context -> Kind -> m ()
-sortCheck _     Type      = return ()
-sortCheck gamma (KPi bnd) =
+sortCheck :: Kind -> TcM Ctx ()
+sortCheck Type      = return ()
+sortCheck (KPi bnd) =
   lunbind bnd $ \((x, Annot a), k) -> do
-    Type <- kCheck gamma a
-    sortCheck (extend gamma x a) k
+    Type <- kCheck a
+    withTmBinding x a $ sortCheck k
 
 ------------------------------------------------------------
 --  Parser  ------------------------------------------------
