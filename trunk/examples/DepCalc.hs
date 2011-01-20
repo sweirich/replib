9073c978b0b5349e8ffdc5776c3fbffbd7dbd843
byorgey <byorgey@d9fc01f3-7ead-27f9-7cc3-93ae016f4439>
2011-01-20 22:05:49 +0000

    a bit more work on DepCalc example and tutorial

--- trunk/examples/DepCalc.hs
+++ trunk/examples/DepCalc.hs
@@ -14,6 +14,14 @@
 
      typing rules:
 
+     x:A \in G
+     -----------
+     G |- x : A
+
+     G, D |- B : *
+     ---------------
+     G |- Pi D.B : *
+
      G, D |- M : B
      ----------------------
      G |- \D.M : Pi D. B
@@ -22,11 +30,14 @@
      ----------------------------------
      G |- M [N] : B[D |-> [N]]
 
-     G |- N : A     (G |- [N] : D)[x |-> N]
+     G |- * : *
+
+
+
+     G |- N : B    A === B    (G |- [N] : D)[x |-> N]
      ----------------------------------
      G |- N, [N] : x:A,D
 
-     G |- * : *
 
 -}
 
@@ -91,25 +102,33 @@ ELam (<(Cons (<<(A,Annot EStar)>> Cons (<<(x,Annot (EVar A))>> Empty)))> EVar 0@
 type M = MaybeT LFreshM
 ok = return ()
 
-tyCheck :: Tele -> Exp -> M Exp
-tyCheck g (EVar x)  = lookUp x g
-tyCheck _ EStar     = return EStar
-tyCheck g (ELam bnd) = do
+infer :: Tele -> Exp -> M Exp
+infer g (EVar x)  = lookUp x g
+infer _ EStar     = return EStar
+infer g (ELam bnd) = do
   lunbind bnd $ \(delta, m) -> do
-    b <- tyCheck (delta `appTele` g) m
+    b <- infer (g `appTele` delta) m
     return . EPi $ bind delta b
-tyCheck g (EApp m ns) = do
-  EPi bnd <- tyCheck g m
+infer g (EApp m ns) = do
+  EPi bnd <- infer g m
   lunbind bnd $ \(delta, b) -> do
     checkList g ns delta
     multiSubst delta ns b
+infer g (EPi bnd) = do
+  lunbind bnd $ \(delta, b) -> do
+    check (g `appTele` delta) b EStar
+    return EStar
+
+check :: Tele -> Exp -> Exp -> M ()
+check g m a = do
+  b <- infer g m
+  checkEq b a
 
 checkList :: Tele -> [Exp] -> Tele -> M ()
 checkList _ [] Empty = ok
 checkList g (e:es) (Cons rb) = do
   let ((x, Annot a), t') = reopen rb
-  b <- tyCheck g e
-  checkEq a b
+  check g e a
   checkList (subst x e g) (subst x e es) (subst x e t')
 checkList _ _ _ = error "Unequal lengths in checkList" -- mismatched lengths
 
@@ -123,3 +142,15 @@ multiSubst _ _ _ = error "Unequal lengths in multiSubst"   -- mismatched lengths
 checkEq :: Exp -> Exp -> M ()
 checkEq e1 e2 = if aeq e1 e2 then return () else error $ "Not equal: " ++ show e1 ++ " " ++ show e2
   -- actually, this is not correct!
+
+{- BUG:
+
+*Main> let Just (EPi bnd) = runLFreshM . runMaybeT $ infer Empty (elam [("A", EStar), ("x", evar "A")] (evar "x"))
+*Main> bnd
+<Cons (<<(A,Annot EStar)>> Cons (<<(x,Annot (EVar 0@0))>> Empty))> EVar 0@0
+*Main> runLFreshM $ lunbind bnd $ \(delta, m) -> error (show delta ++ " " ++ show m)
+*** Exception: Cons (<<(A,Annot EStar)>> Cons (<<(x,Annot (EVar 0@0))>> Empty)) EVar A
+
+The 0@0 in the last line ought to be A.
+
+-}
