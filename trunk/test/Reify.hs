ce0b4dd381c3294353493cae9e2c7665b4aa45b9
byorgey <byorgey@d9fc01f3-7ead-27f9-7cc3-93ae016f4439>
2011-05-31 20:45:18 +0000

    obtaining some info to help with extension to GADTs

--- trunk/test/Reify.hs
+++ trunk/test/Reify.hs
@@ -7,16 +7,58 @@ import Language.Haskell.TH.Syntax
 
 doReify :: Name -> Q [Dec]
 doReify n = do
-  info <- reify n
+  (TyConI dec) <- reify n
   -- let s = show $
   --         case info of
   --           TyConI dec ->
   --             case dec of
   --               (DataD _ _ _ cs _) ->
   --                 map conA cs
-  let s = show info
+  let s = show (typeInfo dec)
   [d| str = $(lift s) |]
 
-conA (NormalC c xs)         = (c, length xs)
-conA (RecC c xs)            = (c, length xs)
-conA (InfixC _ c _)         = (c, 2)
+typeInfo :: Dec -> (Name, [TyVarBndr], [([TyVarBndr], Cxt, Name, Int)], [(Name, [(Maybe Name, Type)])])
+typeInfo d = case d of
+    (DataD _ _ _ _ _) ->
+      (getName d, paramsA d, consA d, termsA d)
+    (NewtypeD _ _ _ _ _) ->
+      (getName d, paramsA d, consA d, termsA d)
+    _ -> error ("derive: not a data type declaration: " ++ show d)
+
+  where
+    consA (DataD _ _ _ cs _)      = map conA cs
+    consA (NewtypeD _ _ _ c _)    = [ conA c ]
+
+    paramsA (DataD _ _ ps _ _)    = ps
+    paramsA (NewtypeD _ _ ps _ _) = ps
+
+    termsA (DataD _ _ _ cs _)     = map termA cs
+    termsA (NewtypeD _ _ _ c _)   = [ termA c ]
+
+    termA (NormalC c xs)          = (c, map (\x -> (Nothing, snd x)) xs)
+    termA (RecC c xs)             = (c, map (\(n, _, t) -> (Just $ simpleName n, t)) xs)
+    termA (InfixC t1 c t2)        = (c, [(Nothing, snd t1), (Nothing, snd t2)])
+    termA (ForallC _ _ n)         = termA n
+
+    conA (NormalC c xs)           = ([], [], simpleName c, length xs)
+    conA (RecC c xs)              = ([], [], simpleName c, length xs)
+    conA (InfixC _ c _)           = ([], [], simpleName c, 2)
+    conA (ForallC bdrs cx con)    = let (bdrs', cx', n, l) = conA con
+                                    in  (bdrs ++ bdrs', cx ++ cx', n, l)
+
+    getName (DataD _ n _ _ _)     = n
+    getName (NewtypeD _ n _ _ _)  = n
+    getName x                     = error $ show x
+
+simpleName :: Name -> Name
+simpleName nm =
+   let s = nameBase nm
+   in case dropWhile (/=':') s of
+        []          -> mkName s
+        _:[]        -> mkName s
+        _:t         -> mkName t
+
+
+tyVarBndrName :: TyVarBndr -> Name
+tyVarBndrName (PlainTV n) = n
+tyVarBndrName (KindedTV n _) = n
