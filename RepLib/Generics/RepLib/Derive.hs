b19037518063a071828075e7de32b54145c1fbe4
byorgey <byorgey@d9fc01f3-7ead-27f9-7cc3-93ae016f4439>
2011-06-08 23:30:36 +0000

    more work on TH generation for GADTs

--- RepLib/Generics/RepLib/Derive.hs
+++ RepLib/Generics/RepLib/Derive.hs
@@ -1,4 +1,8 @@
-{-# LANGUAGE TemplateHaskell, UndecidableInstances #-}
+{-# LANGUAGE TemplateHaskell
+           , UndecidableInstances
+           , TypeOperators
+           , ScopedTypeVariables
+  #-}
 {-# OPTIONS_GHC -fno-warn-incomplete-patterns #-}
 
 -----------------------------------------------------------------------------
@@ -27,8 +31,12 @@ module Generics.RepLib.Derive (
 import Generics.RepLib.R
 import Generics.RepLib.R1
 import Language.Haskell.TH
-import Data.List (nub)
+import Data.List (nub, foldl')
 import Data.Maybe (catMaybes)
+import Data.Type.Equality
+
+import Control.Arrow ((***), second)
+import Control.Applicative ((<$>))
 
 -- | Given a type, produce its representation.
 repty :: Type -> Exp
@@ -62,18 +70,82 @@ rName1 n =
 --    Currently, we don't handle data constructors with record components.
 
 repcon :: Type ->  -- The type that this is a constructor for (applied to all of its parameters)
+          TypeInfo ->    -- information about the type
           ConstrInfo ->  -- information about the contstructor
 	  Q Exp
-repcon d constr
+repcon d info constr
   | null (constrCxt constr) = [| Just $con |]
-  | otherwise               = gadtCase constr con
+  | otherwise               = gadtCase (typeParams info) constr con
   where rargs = foldr (\ t tl -> [| $(return $ repty t) :+: $(tl) |])
                       [| MNil |]
                       (map fieldType . constrFields $ constr)
         con   = [| Con $(remb d constr) $(rargs) |]
 
-gadtCase :: ConstrInfo -> Q Exp -> Q Exp
-gadtCase constr con = undefined -- XXX
+gadtCase :: [TyVarBndr] -> ConstrInfo -> Q Exp -> Q Exp
+gadtCase tyVars constr conQ = do
+  con      <- [| Just $conQ |]
+  (m, pat) <- typeRefinements tyVars constr
+  n        <- [| Nothing |]
+  return $ CaseE m
+    [ Match pat (NormalB con) []
+    , Match WildP (NormalB n) []
+    ]
+
+      -- have to do the above in this long annoying explicit way
+      -- because I don't know how to splice in a pattern =(
+
+      -- this fails with an error:
+
+      -- [| case $(matcher constr) of
+      --     $(successCase constr) ->
+      --     _                     -> Nothing }
+      -- |]
+
+      -- Generics/RepLib/Derive.hs:78:7:
+      --    Parse error in pattern: $(successCase constr)
+
+typeRefinements :: [TyVarBndr] -> ConstrInfo -> Q (Exp, Pat)
+typeRefinements tyVars constr =
+      fmap ((TupE *** TupP) . unzip)
+    . sequence
+    . map genRefinement
+    . catMaybes . map extractLHSVars
+    . catMaybes . map extractEq
+    $ constrCxt constr
+  where extractEq (EqualP ty1 ty2)  = Just (ty1, ty2)
+        extractEq _                 = Nothing
+
+        extractLHSVars (VarT n, t2) | any ((==n) . tyVarBndrName) tyVars = Just (n,t2)
+        extractLHSVars _            = Nothing
+        -- Note, assuming here that equalities involving type parameters
+        -- will always have the type parameter on the LHS...
+
+genRefinement :: (Name, Type) -> Q (Exp, Pat)
+genRefinement (n, ty) = do
+  let (con, args) = decomposeTy ty
+  case args of
+    [] -> do e <- [| eqT (rep :: R $(varT n)) $(return $ repty ty) |]
+             p <- [p| Just Refl |]
+             return (e,p)
+    _  -> do e <- [| $(varE (mkName $ "destr" ++ show (length args)))
+                     (rep :: R $(varT n))
+                     (rep :: R $(appUnits con (length args)))
+                  |]
+             p <- conP (mkName $ "Result" ++ show (length args))
+                       [sigP [p| Refl |] [t| $(varT n) :=: $(return ty) |] ]
+             return (e,p)
+
+-- | Decompose a type into a constructor and a list of arguments.
+decomposeTy :: Type -> (Type, [Type])
+decomposeTy (AppT t1 t2) = second (++[t2]) (decomposeTy t1)
+decomposeTy t = (t, [])
+
+-- | Apply a type constructor to a certain number of copies of the
+-- unit type.
+appUnits :: Type -> Int -> Q Type
+appUnits ty n = do
+  u <- [t| () |]
+  return $ foldl' AppT ty (replicate n u)
 
 -- the "from" function that coerces from an "a" to the arguments
 rfrom :: Type -> ConstrInfo -> Q Exp
@@ -99,7 +171,7 @@ rto _ constr =
   do vars <- mapM (const (newName "x")) (constrFields constr)
      let topat = foldr (\v tl -> InfixP  (VarP v) (mkName ":*:") tl)
                          (ConP 'Nil []) vars
-         tobod = foldl (\tl v -> tl `AppE` (VarE v))
+         tobod = foldl' (\tl v -> tl `AppE` (VarE v))
                        (ConE (simpleName . constrName $ constr))
                        vars
      return (LamE [topat] tobod)
@@ -137,10 +209,10 @@ repr f n = do info' <- reify n
                       constrs    = typeConstrs dInfo
                   baseT <- conT nm
                   -- the type that we are defining, applied to its parameters.
-                  let ty = foldl (\x p -> x `AppT` (VarT p)) baseT paramNames
+                  let ty = foldl' (\x p -> x `AppT` (VarT p)) baseT paramNames
                   -- the representations of the paramters, as a list
                   -- representations of the data constructors
-                  rcons <- mapM (repcon ty) constrs
+                  rcons <- mapM (repcon ty dInfo) constrs
                   body  <- case f of
                      Conc -> [| Data $(repDT nm paramNames)
                                      (catMaybes $(return (ListE rcons))) |]
@@ -159,9 +231,7 @@ repr f n = do info' <- reify n
                   return [rSig, rType, inst]
 
 reprs :: Flag -> [Name] -> Q [Dec]
-reprs f ns = foldl (\qd n -> do decs1 <- repr f n
-                                decs2 <- qd
-                                return (decs1 ++ decs2)) (return []) ns
+reprs f ns = concat <$> mapM (repr f) ns
 
 --------------------------------------------------------------------------------------------
 --- Generating the R1 representation
@@ -209,7 +279,7 @@ repr1 f n = do info' <- reify n
                       nm         = typeName dInfo
                       constrs    = typeConstrs dInfo
                   -- the type that we are defining, applied to its parameters.
-                  let ty = foldl (\x p -> x `AppT` (VarT p)) (ConT nm) paramNames
+                  let ty = foldl' (\x p -> x `AppT` (VarT p)) (ConT nm) paramNames
                   let rTypeName = rName1 n
 
                   ctx <- newName "ctx"
@@ -222,8 +292,8 @@ repr1 f n = do info' <- reify n
                   let cparams = map (\(x,t,_) -> SigP (VarP x) t) ctxParams
 
                   -- the recursive call of the rep function
-                  let e1 = foldl (\a r -> a `AppE` (VarE r)) (VarE rTypeName) paramNames
-                  let e2 = foldl (\a (x,_,_) -> a `AppE` (VarE x)) e1 ctxParams
+                  let e1 = foldl' (\a r -> a `AppE` (VarE r)) (VarE rTypeName) paramNames
+                  let e2 = foldl' (\a (x,_,_) -> a `AppE` (VarE x)) e1 ctxParams
 
                   -- the representations of the parameters, as a list
                   -- representations of the data constructors
@@ -242,8 +312,8 @@ repr1 f n = do info' <- reify n
                   let ctxRep = map (\p -> ClassP (mkName "Rep") [VarT p]) paramNames
                       ctxRec = map (\(_,t,_) -> ClassP ''Sat [t]) ctxParams
 
-                      -- appRep t = foldl (\a p -> a `AppE` (VarE 'rep)) t param
-                      appRec t = foldl (\a _ -> a `AppE` (VarE 'dict)) t ctxParams
+                      -- appRep t = foldl' (\a p -> a `AppE` (VarE 'rep)) t param
+                      appRec t = foldl' (\a _ -> a `AppE` (VarE 'dict)) t ctxParams
 
                   let inst  = InstanceD (ctxRep ++ ctxRec)
                                 ((ConT ''Rep1) `AppT` (VarT ctx) `AppT` ty)
@@ -259,11 +329,7 @@ repr1 f n = do info' <- reify n
 
 
 repr1s :: Flag -> [Name] -> Q [Dec]
-
-
-repr1s f ns = foldl (\qd n -> do decs1 <- repr1 f n
-                                 decs2 <- qd
-                                 return (decs1 ++ decs2)) (return []) ns
+repr1s f ns = concat <$> mapM (repr1 f) ns
 
 -- | Generate representations (both basic and parameterized) for a list of
 -- types.
