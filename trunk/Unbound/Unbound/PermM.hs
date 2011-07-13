90b24f4365aef0960cb7e11dce77126a4ff70073
byorgey <byorgey@d9fc01f3-7ead-27f9-7cc3-93ae016f4439>
2011-07-13 13:52:36 +0000

    new (hopefully correct) implementation of mkPerm

--- trunk/Unbound/Unbound/PermM.hs
+++ trunk/Unbound/Unbound/PermM.hs
@@ -10,14 +10,17 @@
 ----------------------------------------------------------------------
 
 module Unbound.PermM (
-    Perm(..), single, compose, apply, support, isid, join, empty, restrict, mkPerm
+    Perm(..), permValid, single, compose, apply, support, isid, join, empty, restrict, mkPerm
   ) where
 
 import Data.Monoid
 import Data.List
 import Data.Map (Map)
 import Data.Function (on)
-import qualified Data.Map as Map
+import qualified Data.Map as M
+import qualified Data.Set as S
+import Control.Arrow ((&&&))
+import Control.Monad ((>=>))
 
 -- | A /permutation/ is a bijective function from names to names
 --   which is the identity on all but a finite set of names.  They
@@ -25,33 +28,39 @@ import qualified Data.Map as Map
 --   also be useful in general.
 newtype Perm a = Perm (Map a a)
 
+permValid :: Ord a => Perm a -> Bool
+permValid (Perm p) = all (\(_,v) -> M.member v p) (M.assocs p)
+  -- a Map sends every key uniquely to a value by construction.  So if
+  -- every value is also a key, the sizes of the domain and range must
+  -- be equal and hence the mapping is a bijection.
+
 instance Ord a => Eq (Perm a) where
   (Perm p1) == (Perm p2) =
-    all (\x -> Map.findWithDefault x x p1 == Map.findWithDefault x x p2) (Map.keys p1) &&
-    all (\x -> Map.findWithDefault x x p1 == Map.findWithDefault x x p2) (Map.keys p2)
+    all (\x -> M.findWithDefault x x p1 == M.findWithDefault x x p2) (M.keys p1) &&
+    all (\x -> M.findWithDefault x x p1 == M.findWithDefault x x p2) (M.keys p2)
 
 instance Show a => Show (Perm a) where
   show (Perm p) = show p
 
 -- | Apply a permutation to an element of the domain.
 apply :: Ord a => Perm a -> a -> a
-apply (Perm p) x = Map.findWithDefault x x p
+apply (Perm p) x = M.findWithDefault x x p
 
 -- | Create a permutation which swaps two elements.
 single :: Ord a => a -> a -> Perm a
-single x y = if x == y then Perm Map.empty else
-    Perm (Map.insert x y (Map.insert y x Map.empty))
+single x y = if x == y then Perm M.empty else
+    Perm (M.insert x y (M.insert y x M.empty))
 
 -- | The empty (identity) permutation.
 empty :: Perm a
-empty = Perm Map.empty
+empty = Perm M.empty
 
 -- | Compose two permutations.  The right-hand permutation will be
 --   applied first.
 compose :: Ord a => Perm a -> Perm a -> Perm a
 compose (Perm b) (Perm a) =
-  Perm (Map.fromList ([ (x,Map.findWithDefault y y b) | (x,y) <- Map.toList a]
-         ++ [ (x, Map.findWithDefault x x b) | x <- Map.keys b, Map.notMember x a]))
+  Perm (M.fromList ([ (x,M.findWithDefault y y b) | (x,y) <- M.toList a]
+         ++ [ (x, M.findWithDefault x x b) | x <- M.keys b, M.notMember x a]))
 
 -- | Permutations form a monoid under composition.
 instance Ord a => Monoid (Perm a) where
@@ -61,26 +70,54 @@ instance Ord a => Monoid (Perm a) where
 -- | Is this the identity permutation?
 isid :: Ord a => Perm a -> Bool
 isid (Perm p) =
-     Map.foldrWithKey (\ a b r -> r && a == b) True p
+     M.foldrWithKey (\ a b r -> r && a == b) True p
 
 -- | /Join/ two permutations by taking the union of their relation
 --   graphs. Fail if they are inconsistent, i.e. map the same element
 --   to two different elements.
 join :: Ord a => Perm a -> Perm a -> Maybe (Perm a)
 join (Perm p1) (Perm p2) =
-     let overlap = Map.intersectionWith (==) p1 p2 in
-     if Map.fold (&&) True overlap then
-       Just (Perm (Map.union p1 p2))
+     let overlap = M.intersectionWith (==) p1 p2 in
+     if M.fold (&&) True overlap then
+       Just (Perm (M.union p1 p2))
        else Nothing
 
 -- | The /support/ of a permutation is the set of elements which are
 --   not fixed.
 support :: Ord a => Perm a -> [a]
-support (Perm p) = [ x | x <- Map.keys p, Map.findWithDefault x x p /= x]
+support (Perm p) = [ x | x <- M.keys p, M.findWithDefault x x p /= x]
 
 -- | Restrict a permutation to a certain domain.
 restrict :: Ord a => Perm a -> [a] -> Perm a
-restrict (Perm p) l = Perm (foldl' (\p' k -> Map.delete k p') p l)
+restrict (Perm p) l = Perm (foldl' (\p' k -> M.delete k p') p l)
+
+-- | A partial permutation consists of two maps, one in each direction
+--   (inputs -> outputs and outputs -> inputs).
+data PartialPerm a = PP (M.Map a a) (M.Map a a)
+  deriving Show
+
+emptyPP :: PartialPerm a
+emptyPP = PP M.empty M.empty
+
+extendPP :: Ord a => a -> a -> PartialPerm a -> Maybe (PartialPerm a)
+extendPP x y pp@(PP mfwd mrev)
+  | Just y' <- M.lookup x mfwd = if y == y' then Just pp
+                                            else Nothing
+  | Just x' <- M.lookup y mrev = if x == x' then Just pp
+                                            else Nothing
+  | otherwise = Just $ PP (M.insert x y mfwd) (M.insert y x mrev)
+
+-- | Convert a partial permutation into a full permutation by closing
+--   off any remaining open chains into a cycles.
+ppToPerm :: Ord a => PartialPerm a -> Perm a
+ppToPerm (PP mfwd mrev) = Perm $ foldr (uncurry M.insert) mfwd
+                                       (map (findEnd &&& id) chainStarts)
+        -- beginnings of open chains are elements which map to
+        -- something in the forward direction but have no ancestor.
+  where chainStarts = S.toList (M.keysSet mfwd `S.difference` M.keysSet mrev)
+        findEnd x = case M.lookup x mfwd of
+                      Nothing -> x
+                      Just x' -> findEnd x'
 
 -- | @mkPerm l1 l2@ creates a permutation that sends @l1@ to @l2@.
 --   Fail if there is no such permutation, either because the lists
@@ -88,10 +125,6 @@ restrict (Perm p) l = Perm (foldl' (\p' k -> Map.delete k p') p l)
 --   can only happen if @l1@ or @l2@ have repeated elements).
 mkPerm :: Ord a => [a] -> [a] -> Maybe (Perm a)
 mkPerm xs ys
-  | inconsistent xs ys     = Nothing
-  | otherwise              = Just . mconcat $ zipWith single xs ys
-  where inconsistent xs ys = length xs /= length ys ||
-          (any (not . repeated) . (map . map) snd . groupBy ((==) `on` fst) . sort $ zip xs ys)
-        repeated = singletonList . group
-        singletonList [_] = True
-        singletonList _   = False
+  | length xs /= length ys = Nothing
+  | otherwise =
+    fmap ppToPerm . ($emptyPP) . foldr (>=>) return $ zipWith extendPP xs ys
